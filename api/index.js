990345aaf4aa718dc0b51d9bd2debffc489e1563
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2023-12-12 18:16:50 +0200

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log('connected to Mongo server');
+  })
+  .catch((err) => console.log('error connecting to Mongo server', err));
 const app = express();
 
 app.listen(3000, () => {
