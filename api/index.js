d30b4382541d1ad8294e433d9b2a7229991f6883
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2023-12-12 18:47:20 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_DB)
@@ -13,3 +14,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('listening on 3000');
 });
+
+app.use('/api/user', userRouter);
