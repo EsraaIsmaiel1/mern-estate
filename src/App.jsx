51ca9b99afd452e2fa960331efdc42183d9cac53
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2023-12-12 17:33:02 +0200

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import About from './pages/About.jsx';
 import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
