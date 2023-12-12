4538cfe34f844893b596fd5d72e91a29e4dde59a
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2023-12-12 19:15:42 +0200

    create signUp api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_DB)
@@ -10,9 +11,11 @@ mongoose
   })
   .catch((err) => console.log('error connecting to Mongo server', err));
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('listening on 3000');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
