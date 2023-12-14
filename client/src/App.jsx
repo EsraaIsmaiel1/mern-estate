c1ad1cfaf02d988fcf6303775b90d348b12427d9
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2023-12-14 21:43:24 +0200

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About.jsx';
 import Profile from './pages/Profile.jsx';
 import Header from './components/Header.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import CreateListing from './pages/createListing.jsx';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   signOutUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -177,6 +178,12 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 uppercase rounded-lg text-center hover:opacity-95"
+          to="/create-listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
